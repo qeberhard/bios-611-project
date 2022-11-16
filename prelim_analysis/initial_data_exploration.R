2159a666307185db0a45dc67bcf42f41af64740c
Quinn Eberhard <qeberha@ad.unc.edu>
2022-11-16 05:56:11 +0000

    Fixed makefile and initial_data_exploration.R file placement bugs

--- prelim_analysis/initial_data_exploration.R
+++ prelim_analysis/initial_data_exploration.R
@@ -1,20 +1,23 @@
 library(ggplot2)
 library(stringr)
+library(readr)
 
 
 #read in data
-txptome <- read.csv("../Data/5mers_expressed_txpts.csv")
+txptome1 <- read_csv("source_data/5mers_expressed_txpts_1.csv.gz")
+txptome2 <- read_csv("source_data/5mers_expressed_txpts_2.csv.gz")
+txptome <- rbind(txptome1, txptome2)
 colnames(txptome)[1] <- "RNA"
-head(txptome)
+#head(txptome)
 
 #Initial visualization of the ACACA motif freq
-png("../figures/ACACA_motif_txptome.png")
+png("figures/ACACA_motif_txptome.png")
 ggplot(txptome, aes(x=ACACA)) + geom_histogram(binwidth=0.05)
 dev.off()
 
 #Visualizing highly expressed ACACA motif freq
 expressed_ACACA <- subset(txptome, txptome$ACACA >= 3)
-png("../figures/ACACA_motif_expressed.png")
+png("figures/ACACA_motif_expressed.png")
 ggplot(expressed_ACACA, aes(x=ACACA)) + geom_histogram(binwidth=0.05)
 dev.off()
 ACACA_rnas <- expressed_ACACA$RNA
@@ -22,13 +25,13 @@ ACACA_rnas <- expressed_ACACA$RNA
 #Identify the percentage of unspliced txpts that highly express ACACA
 unspliced_ACACA_ids <- str_extract(expressed_ACACA$RNA, "unspliced")
 unspliced_ACACA <- subset(expressed_ACACA, unspliced_ACACA_ids == "unspliced")
-length(unspliced_ACACA$RNA)/length(expressed_ACACA$RNA)
+#length(unspliced_ACACA$RNA)/length(expressed_ACACA$RNA)
 
 #Identify total unspliced transcriptome
 unspliced_txptome_ids <- str_extract(txptome$RNA, "unspliced")
 unspliced_txptome <- subset(txptome, unspliced_txptome_ids == "unspliced")
 unspliced_txptome_freq <- length(unspliced_txptome$RNA)/length(txptome$RNA)
-unspliced_txptome_freq
+#unspliced_txptome_freq
 write.csv(unspliced_txptome,"derived_data/unspliced_txptome.csv", row.names = FALSE)
 
 #Identify the transcripts of XIST
